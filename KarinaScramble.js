e49c68ea90979dbeae43c0a8588e82316e24d4b1
KrallB <47430276+KrallB@users.noreply.github.com>
2023-12-20 23:42:25 -0800

    another upload

---
+++ KarinaScramble.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //Adam
+    const correctOrder = "Gifts & Laughter".replace(/\s/g, '').toUpperCase();
+    const letterBoxes = document.querySelectorAll('.letter-box');
+
+    letterBoxes.forEach((box, index) => {
+        box.addEventListener('input', () => {
+            const currentLetter = box.value.toUpperCase();
+
+            if (currentLetter === correctOrder[index]) {
+                box.classList.add('correct');
+                box.classList.remove('incorrect');
+                if (index < letterBoxes.length - 1) {
+                    letterBoxes[index + 1].focus();
+                }
+            } else {
+                box.classList.add('incorrect');
+                box.classList.remove('correct');
+                box.value = '';
+            }
+        });
+    });
+});
